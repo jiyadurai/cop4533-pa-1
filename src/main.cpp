502a1eb33262e8202f41da7c20afe7e4897a65ac
23jiyadurai <jeevan.iyadurai@gmail.com>
2026-01-30 10:47:25 -0500

    Adds classes for hospital and student and matching, the types that will be used by the matching function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,64 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+
+struct Hospital {
+    std::vector<int> preferenceList;
+    std::unordered_map<int, int> preferences;
+    int id;
+    explicit Hospital(int id) {
+        this->id = id;
+    }
+};
+
+struct Student {
+    std::vector<int> preferenceList;
+    std::unordered_map<int, int> preferences;
+    int id;
+    explicit Student(int id) {
+        this->id = id;
+    }
+};
+
+struct Matching {
+    std::vector<std::pair<int, int>> pairs;
+};
+
+Hospital readHospital(int i, int N) {
+    Hospital h{i};
+    h.preferenceList.reserve(N);
+    for (int j = 1; j <= N; j++) {
+        int x;
+        std::cin >> x;
+        h.preferences[j] = x;
+        h.preferenceList.emplace_back(x);
+    }
+    return h;
+}
+
+Student readStudent(int i, int N) {
+    Student s{i};
+    s.preferenceList.reserve(N);
+    for (int j = 1; j <= N; j++) {
+        int x;
+        std::cin >> x;
+        s.preferences[j] = x;
+        s.preferenceList.emplace_back(x);
+    }
+    return s;
+}
 
 int main() {
-    std::cout << "hello world\n";
+    int N;
+    std::cin >> N;
+    std::vector<Hospital> hospitals;
+    std::vector<Student> students;
+    for (int i = 1; i <= N; i++) {
+        hospitals.push_back(readHospital(i, N));
+    }
+    for (int i = 1; i <= N; i++) {
+        students.push_back(readStudent(i, N));
+    }
+
 }
